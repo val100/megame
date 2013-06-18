8f7ad48cc5df2acc4a51f996fe627b893a374232
val <val@home-7c3847abbe>
2013-06-19 00:28:34 +0300

    Add model objects

---
+++ src/java/com/nadeem/battleship/Board.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nadeem.battleship;
+
+/**
+ *
+ * @author val
+ */
+public class Board {
+    
+}
