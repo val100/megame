7491e7e94ea3105ba0e6ba3b4de56c84a2f3f73d
val <val@home-7c3847abbe>
2013-06-21 22:47:46 +0300

    - added 'inCell()' - added field sunk for sunk ship - initialized damage to 0

--- src/java/com/nadeem/battleship/Ship.java
+++ src/java/com/nadeem/battleship/Ship.java
@@ -1,49 +1,112 @@
 package com.nadeem.battleship;
+
 /**
  * The Ship class models each individual ship of the game board.
+ *
  * @author nadeem
  */
 public class Ship {
+
     private int size;   // ship size
     private int damage; // ship damage counter
     private Cell cell;  // ship start cell
+    private boolean sunk; // ship sunk status
     private Direction direction; // ship direction , horizontal or verical
     // ship size constants 
-    public static final int   CARRIER = 4;
-    public static final int   CRUISER = 3;
+    public static final int CARRIER = 4;
+    public static final int CRUISER = 3;
     public static final int DESTROYER = 2;
-    public static final int    PATROL = 1;
+    public static final int PATROL = 1;
+
     /**
-     * Constructor to initialize this ship   
-     * 
+     * Constructor to initialize this ship
+     *
      * @param size the ship size
      */
     public Ship(int size) {
         this.size = size;
-        this.damage = size;
         this.direction = Direction.HORIZONTAL;
+        this.damage = 0;
+        this.sunk = false;
+    }
+
+    public Ship(int size, Cell cell, Direction direction) {
+        this.size = size;
+        this.cell = cell;
+        this.direction = direction;
+        this.damage = 0;
+        this.sunk = false;
     }
 
     public Cell getCell() {
         return cell;
     }
-    
+
     public Direction getDirection() {
         return direction;
     }
-    
+
     public int getSize() {
         return size;
     }
-    
+
     public int getDamage() {
         return damage;
+    }
+
+    public void incDamage() {
+        damage++;
+        if (damage == size) {
+            setSunk(true);
+        }
+    }
+
+    public boolean isSunk() {
+        return sunk;
+    }
+
+    public void setSunk(boolean sunk) {
+        this.sunk = sunk;
     }
 
     public void setCoords(Cell cell, Direction direction) {
         this.cell = cell;
         this.cell.setStatus(Status.SHIP);
         this.direction = direction;
+    }
+
+    /**
+     * Check if the given cell has been occupied with ship. 
+     *
+     * @param c the cell to check
+     * @return true if cell has been occupied with ship , otherwise false
+     */
+    public boolean inCell(Cell c) {
+        if (cell == null) {
+            return false;
+        }
+        // if given cell equals to start cell of pplaced ship
+        if (cell.equals(c)) {
+            return true;
+        // else check if the given cell occupied with ship
+        } else {
+            Cell checkCell = c;
+            if (direction == Direction.HORIZONTAL) {
+                if ((checkCell.getX() >= cell.getX() && checkCell.getX() <= cell.getX() + size)
+                        && checkCell.getY() == cell.getY()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                if ((checkCell.getY() >= cell.getY() && checkCell.getY() <= cell.getY() + size)
+                        && checkCell.getX() == cell.getX()) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
     }
 
     @Override
@@ -79,6 +142,4 @@ public class Ship {
         hash = 73 * hash + (this.direction != null ? this.direction.hashCode() : 0);
         return hash;
     }
-    
-    
 }
