a02e72abc8500bf79cd7527c8a96e740ab97376c
val <val@home-7c3847abbe>
2013-06-21 22:52:33 +0300

    - the main class was not changed

--- src/java/com/nadeem/battleship/Main.java
+++ src/java/com/nadeem/battleship/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Board board = new Board();
         board.showBoard();
-        board.placeShipRandom();
+        board.placeShipRandomly();
         board.showBoard();
     }
 }
